f6bb4254468e5d351edb9564ef1c920a652ddda9
brempeedimento-byte <br.empeedimento@gmail.com>
2025-12-22 21:26:25 +0000

    Estrutura Crítica: Adicionando dependências e diretórios base para o Build

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{background:'#000', color:'#0f0', height:'100vh', display:'flex', alignItems:'center', justifyContent:'center', fontFamily:'monospace'}}>
+      <h1>SISTEMA BR DELIVERY EXPRESS: AGUARDANDO COMANDO DO CEO</h1>
+    </div>
+  )
+}
